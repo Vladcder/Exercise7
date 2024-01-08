8023ab434a7d1b18981c9d9cc9165fb3c9223215
Vlad <gresko.97@mail.ru>
2024-01-08 17:09:16 +0300

    Add project files.

---
+++ Task1/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] squares = new double[2];
+            double a;
+            double b ;
+            double c;
+
+            for (int i = 0; i < squares.Length; i++)
+            {
+                Console.WriteLine($"Введите размеры сторон треугольника {i+1}");
+                 a = Convert.ToDouble(Console.ReadLine());
+                 b = Convert.ToDouble(Console.ReadLine());
+                 c = Convert.ToDouble(Console.ReadLine());
+                if (CheckTriangel(a, b, c))
+                {
+                    squares[i] = CalcTriangleSquare(a, b, c);
+                }
+                else
+                {
+                    Console.WriteLine("Треугольник не возможный");
+                    Console.ReadKey();
+                    return;
+                }
+            }
+       if (squares[0] > squares[1])
+            {
+                Console.WriteLine("Первый треугольник больше");
+            }
+       else if (squares[0] < squares[1])
+            {
+                Console.WriteLine("Второй треугольник больше");
+            }
+       else
+            {
+                Console.WriteLine("Треугольники равны");
+            }
+
+       Console.ReadKey();
+        }
+
+        static double CalcTriangleSquare(double x, double y, double z) 
+        {
+            double p = x + y + z;
+            double halfP = p / 2;
+            var sq= Math.Sqrt(halfP * (halfP - x) * (halfP - y) * (halfP - z));
+            return sq;
+        }
+
+        static bool CheckTriangel(double x, double y, double z)
+        {
+            if (x+y>z&&y+z>x&&x+z>y)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+
+            }
+
+        }
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Введите размер грани куба");
+            double v = 0;
+            double s = 0;
+            var a = Convert.ToDouble(Console.ReadLine());
+            CalculateParams(a, out v, out s);
+            Console.WriteLine($"Площадь куба равна {s}, объем куба равен {v}");
+            Console.ReadKey();
+        }
+
+
+        static void CalculateParams(double a, out double v, out double s)
+        {
+            v = a * a * a;
+            s = 6 * a * a;
+        }
+    }
+}
